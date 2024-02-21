e496c3737841b878b3f698de231c9711f06e1b35
Darshil Dobariya <89260736+darshil44@users.noreply.github.com>
2024-02-21 12:04:58 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,23 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
     const btn = document.querySelector('.changeColorBtn');
+    const colorGrid = document.querySelector('.colorGrid');
+    const colorValue = document.querySelector('.colorValue');
 
     btn.addEventListener('click', async () => {
         let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
         console.log(tab);
 
-chrome.scripting.executeScript({
+        chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: pickColor,
-        })
-    }); 
+        },
+            async (injectionResults) => {
+                const [data] = injectionResults;
+                if (data.result) {
+                    const color = data.result.sRGBHex;
+                    colorGrid.style.backgroundColor = color;
+                    colorValue.innerText = color;
+                    try {
+                        await navigator.clipboard.writeText(color);
+                    } catch (err) {
+                        console.err(err)
+                    }
+                }
+                console.log(injectionResults);
+            }
 
-    function pickColor() {
-        console.log('script working');
+        );
+    });
+
+    async function pickColor() {
+        try {
+            const eyeDropper = new EyeDropper();
+            const selectedColor = await eyeDropper.open();
+            return selectedColor;
+        } catch (err) {
+            console.log(err);
+        }
     }
-
-
-
-
-
 });
-
